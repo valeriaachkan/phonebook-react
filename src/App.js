020abdaa7d0156ef687a57b85d454fa9c81ed350
valeriaachkan <91270873+valeriaachkan@users.noreply.github.com>
2023-08-26 13:16:32 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -6,44 +6,6 @@ import ContactList from './components/ContactList';
 import Filter from './components/Filter';
 
 export const App = () => {
-  // const [contacts, setContacts] = useState(() =>
-  //   JSON.parse(window.localStorage.getItem('contacts') ?? []),
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(
-  //   () => window.localStorage.setItem('contacts', JSON.stringify(contacts)),
-  //   [contacts],
-  // );
-
-  // const addNewContact = ({ name, number }) => {
-  //   const newContact = {
-  //     id: nanoid(),
-  //     name,
-  //     number,
-  //   };
-
-  //   setContacts(contacts => [...contacts, newContact]);
-  // };
-
-  // const deleteContact = contactId => {
-  //   setContacts(contacts => contacts.filter(({ id }) => id !== contactId));
-  // };
-
-  // const handleChangeFilter = e => {
-  //   setFilter(e.currentTarget.value);
-  // };
-
-  // const filteredContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase().trim();
-
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(normalizedFilter),
-  //   );
-  // };
-
-  // const filteredContactList = filteredContacts();
-
   return (
     <>
       <Section title={'Phonebook'}>
@@ -57,3 +19,41 @@ export const App = () => {
     </>
   );
 };
+
+// const [contacts, setContacts] = useState(() =>
+//   JSON.parse(window.localStorage.getItem('contacts') ?? []),
+// );
+// const [filter, setFilter] = useState('');
+
+// useEffect(
+//   () => window.localStorage.setItem('contacts', JSON.stringify(contacts)),
+//   [contacts],
+// );
+
+// const addNewContact = ({ name, number }) => {
+//   const newContact = {
+//     id: nanoid(),
+//     name,
+//     number,
+//   };
+
+//   setContacts(contacts => [...contacts, newContact]);
+// };
+
+// const deleteContact = contactId => {
+//   setContacts(contacts => contacts.filter(({ id }) => id !== contactId));
+// };
+
+// const handleChangeFilter = e => {
+//   setFilter(e.currentTarget.value);
+// };
+
+// const filteredContacts = () => {
+//   const normalizedFilter = filter.toLowerCase().trim();
+
+//   return contacts.filter(({ name }) =>
+//     name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
+
+// const filteredContactList = filteredContacts();
--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter basename="/phonebook-react">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
